0ccdecb2efb7de394cea07677dadedcf5ecbd167
chenJ <582699393@qq.com>
2024-04-24 01:54:32 +0800

    feat: 增加数据库配置信息，增加初始化SQL

--- model/src/main/java/com/chenJ/model/base/BaseEntity.java
+++ model/src/main/java/com/chenJ/model/base/BaseEntity.java
@@ -5,7 +5,6 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import lombok.Data;
-import org.apache.ibatis.annotations.Update;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -25,13 +24,9 @@ public class BaseEntity implements Serializable {
     @TableId(type = IdType.AUTO)
     private Long id;
 
-    @TableField("create_user")
-    private String  createUser;
     @TableField("create_time")
     private Date createTime;
 
-    @TableField("create_user")
-    private String  updateUser;
     @TableField("update_time")
     private Date updateTime;
 
---
+++ service-oa/src/main/java/com/chenJ/auth/ChenJApplication.java
@@ -0,0 +1,21 @@
+package com.chenJ.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : chenJ
+ * @Project : chenJ-oa-parent
+ * @Package : com.chenJ.auth.mapper
+ * @ClassName : ChenJApplication.java
+ * @createTime : 2024/4/24 1:35
+ * @Description :
+ */
+@SpringBootApplication
+public class ChenJApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChenJApplication.class);
+    }
+}
